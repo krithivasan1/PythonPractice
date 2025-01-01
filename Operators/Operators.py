b195756ea2ff8996c764fec2b0e589f4136425a2
krithivasan1 <krithi.vn@gmail.com>
2025-01-01 21:27:15 +0530

    included the operators

---
+++ Operators/Operators.py
@@ -0,0 +1,19 @@
+# concatenation 
+print('a'+'a'+'a')
+# another form of concatenation 
+print ('a'*3)
+# both prints aaa 
+x=10
+y=5
+print(x,y)
+print(y-x) # -5
+
+print(5-2.0) # 3.0
+print(x/y ) # 2.0
+
+# power 
+print (x**2) #100 
+
+# floor operator
+print(x/2.2)
+print(x//2.2)
